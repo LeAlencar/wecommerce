2ddf67e62670392aab390aeb2ee85d2be0c6e3fe
Leandro Alencar <zzleandrobritozz@gmail.com>
2023-12-22 07:23:09 -0300

    chore(package): update packages name

--- apps/docs/src/app/layout.tsx
+++ apps/docs/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import "./globals.css";
-import "@repo/ui/styles.css";
+import "@wecommerce/ui/styles.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
--- apps/docs/tailwind.config.ts
+++ apps/docs/tailwind.config.ts
@@ -1,7 +1,7 @@
 // tailwind config is required for editor support
 
 import type { Config } from "tailwindcss";
-import sharedConfig from "@repo/tailwind-config";
+import sharedConfig from "@wecommerce/tailwind-config";
 
 const config: Pick<Config, "content" | "presets"> = {
   content: ["./src/app/**/*.tsx"],
