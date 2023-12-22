fc6edc590255d799d5e237556678210386da4c25
Leandro Alencar <zzleandrobritozz@gmail.com>
2023-12-22 08:14:58 -0300

    feat(ui): add shadcn & radix to ui-package

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,6 +1,10 @@
+import { Button } from '@wecommerce/ui'
+
 export default function Page(): JSX.Element {
   return (
-    <>
-    </>
+    <div className='mt-5 flex justify-center'>
+
+      <Button className='size-24 bg-red-100 rounded-sm'>Button</Button>
+    </div>
   );
 }
