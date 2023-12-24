3a1c74157fd3dc3fcffde4e05eed35e09a26a86e
Leandro Alencar <zzleandrobritozz@gmail.com>
2023-12-24 16:44:51 -0300

    fix(middleware): too many redirects

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,23 +1,32 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { cookies } from "next/headers";
-// This function can be marked `async` if using `await` inside
+
 export function middleware(request: NextRequest) {
+  const url = request.nextUrl.clone();
   const user = cookies().get("userToken");
+
+  if (
+    !user &&
+    (url.pathname.startsWith("/login") || url.pathname.startsWith("/register"))
+  ) {
+    return NextResponse.next();
+  }
+
   if (!user) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
 
   if (
-    request.nextUrl.pathname.startsWith("/register") ||
-    request.nextUrl.pathname.startsWith("/login")
+    url.pathname.startsWith("/login") ||
+    url.pathname.startsWith("/register")
   ) {
     return NextResponse.redirect(new URL("/", request.url));
   }
+
   return NextResponse.next();
 }
 
-// See "Matching Paths" below to learn more
 export const config = {
   matcher: "/((?!api|_next/static|_next/image|favicon.ico).*)",
 };
