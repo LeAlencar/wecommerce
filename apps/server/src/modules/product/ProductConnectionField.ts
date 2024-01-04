69dfccceb4a2d51cead0b67872ce9fafb63815c8
Leandro Alencar <zzleandrobritozz@gmail.com>
2024-01-04 19:40:31 -0300

    test(product): product add mutation test

--- apps/server/src/modules/product/ProductConnectionField.ts
+++ apps/server/src/modules/product/ProductConnectionField.ts
@@ -5,15 +5,18 @@ import {
   withFilter
 } from '@entria/graphql-mongo-helpers';
 import * as ProductLoader from './ProductLoader';
-import { GraphQLNonNull } from 'graphql';
+import { GraphQLNonNull, GraphQLString } from 'graphql';
 
 export const productConnectionField = () => ({
   products: {
     type: new GraphQLNonNull(ProductConnection),
     args: {
-      ...connectionArgs
+      ...connectionArgs,
+      id: {
+        type: GraphQLString
+      }
     },
-    resolve: (obj, args, context) => {
+    resolve: async (obj, args, context) => {
       if (!context.user) {
         return NullConnection;
       }
