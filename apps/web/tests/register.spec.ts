0faaaf3664b816fe903217642ae5d2c9e3166e3e
Leandro Alencar <zzleandrobritozz@gmail.com>
2024-01-15 21:30:24 -0300

    fix tests

--- apps/web/tests/register.spec.ts
+++ apps/web/tests/register.spec.ts
@@ -16,10 +16,11 @@ test("should display the register form", async ({ page }) => {
   expect(loginButton).toBeTruthy();
 });
 
-test("should be able to fill login form", async ({ page }) => {
+test("should be able to fill register form", async ({ page }) => {
   await page.goto("http://localhost:3000/register");
   await page.fill('input[name="email"]', "user@example.com");
   await page.fill('input[name="username"]', "user123");
   await page.fill('input[name="password"]', "password");
+  await page.fill('input[name="pixKey"]', "pix-key");
   await page.click('button[type="submit"]');
 });
