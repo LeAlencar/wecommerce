364ed685a5dd8f82975764238cf0b20ee6d07306
Leandro Alencar <zzleandrobritozz@gmail.com>
2024-01-17 23:17:17 -0300

    fix(relay): typo

--- apps/web/src/app/charges/__generated__/pageChargesQuery.graphql.ts
+++ apps/web/src/app/charges/__generated__/pageChargesQuery.graphql.ts
@@ -8,30 +8,20 @@
 /* eslint-disable */
 // @ts-nocheck
 
-import { ConcreteRequest, Query } from "relay-runtime";
+import { ConcreteRequest, Query } from 'relay-runtime';
 export type pageChargesQuery$variables = Record<PropertyKey, never>;
 export type pageChargesQuery$data = {
   readonly charges: {
-    readonly edges:
-      | ReadonlyArray<
-          | {
-              readonly node:
-                | {
-                    readonly brCode: string;
-                    readonly customerEmail: string;
-                    readonly product: {
-                      readonly name: string;
-                      readonly price: number;
-                    };
-                  }
-                | null
-                | undefined;
-            }
-          | null
-          | undefined
-        >
-      | null
-      | undefined;
+    readonly edges: ReadonlyArray<{
+      readonly node: {
+        readonly brCode: string;
+        readonly customerEmail: string;
+        readonly product: {
+          readonly name: string;
+          readonly price: number;
+        };
+      } | null | undefined;
+    } | null | undefined> | null | undefined;
   };
 };
 export type pageChargesQuery = {
@@ -39,231 +29,240 @@ export type pageChargesQuery = {
   variables: pageChargesQuery$variables;
 };
 
-const node: ConcreteRequest = (function () {
-  var v0 = {
-      alias: null,
-      args: null,
-      kind: "ScalarField",
-      name: "brCode",
-      storageKey: null,
+const node: ConcreteRequest = (function(){
+var v0 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "brCode",
+  "storageKey": null
+},
+v1 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "customerEmail",
+  "storageKey": null
+},
+v2 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "name",
+  "storageKey": null
+},
+v3 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "price",
+  "storageKey": null
+},
+v4 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "__typename",
+  "storageKey": null
+},
+v5 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "cursor",
+  "storageKey": null
+},
+v6 = {
+  "alias": null,
+  "args": null,
+  "concreteType": "PageInfo",
+  "kind": "LinkedField",
+  "name": "pageInfo",
+  "plural": false,
+  "selections": [
+    {
+      "alias": null,
+      "args": null,
+      "kind": "ScalarField",
+      "name": "endCursor",
+      "storageKey": null
     },
-    v1 = {
-      alias: null,
-      args: null,
-      kind: "ScalarField",
-      name: "customerEmail",
-      storageKey: null,
-    },
-    v2 = {
-      alias: null,
-      args: null,
-      kind: "ScalarField",
-      name: "name",
-      storageKey: null,
-    },
-    v3 = {
-      alias: null,
-      args: null,
-      kind: "ScalarField",
-      name: "price",
-      storageKey: null,
-    },
-    v4 = {
-      alias: null,
-      args: null,
-      kind: "ScalarField",
-      name: "__typename",
-      storageKey: null,
-    },
-    v5 = {
-      alias: null,
-      args: null,
-      kind: "ScalarField",
-      name: "cursor",
-      storageKey: null,
-    },
-    v6 = {
-      alias: null,
-      args: null,
-      concreteType: "PageInfo",
-      kind: "LinkedField",
-      name: "pageInfo",
-      plural: false,
-      selections: [
-        {
-          alias: null,
-          args: null,
-          kind: "ScalarField",
-          name: "endCursor",
-          storageKey: null,
-        },
-        {
-          alias: null,
-          args: null,
-          kind: "ScalarField",
-          name: "hasNextPage",
-          storageKey: null,
-        },
-      ],
-      storageKey: null,
-    },
-    v7 = [
+    {
+      "alias": null,
+      "args": null,
+      "kind": "ScalarField",
+      "name": "hasNextPage",
+      "storageKey": null
+    }
+  ],
+  "storageKey": null
+},
+v7 = [
+  {
+    "kind": "Literal",
+    "name": "first",
+    "value": 100
+  }
+],
+v8 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "id",
+  "storageKey": null
+};
+return {
+  "fragment": {
+    "argumentDefinitions": [],
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "pageChargesQuery",
+    "selections": [
       {
-        kind: "Literal",
-        name: "first",
-        value: 100,
-      },
-    ],
-    v8 = {
-      alias: null,
-      args: null,
-      kind: "ScalarField",
-      name: "id",
-      storageKey: null,
-    };
-  return {
-    fragment: {
-      argumentDefinitions: [],
-      kind: "Fragment",
-      metadata: null,
-      name: "pageChargesQuery",
-      selections: [
-        {
-          alias: "charges",
-          args: null,
-          concreteType: "ChargeConnection",
-          kind: "LinkedField",
-          name: "__ChargeList_charges_connection",
-          plural: false,
-          selections: [
-            {
-              alias: null,
-              args: null,
-              concreteType: "ChargeEdge",
-              kind: "LinkedField",
-              name: "edges",
-              plural: true,
-              selections: [
-                {
-                  alias: null,
-                  args: null,
-                  concreteType: "Charge",
-                  kind: "LinkedField",
-                  name: "node",
-                  plural: false,
-                  selections: [
-                    v0 /*: any*/,
-                    v1 /*: any*/,
-                    {
-                      alias: null,
-                      args: null,
-                      concreteType: "Product",
-                      kind: "LinkedField",
-                      name: "product",
-                      plural: false,
-                      selections: [v2 /*: any*/, v3 /*: any*/],
-                      storageKey: null,
-                    },
-                    v4 /*: any*/,
-                  ],
-                  storageKey: null,
-                },
-                v5 /*: any*/,
-              ],
-              storageKey: null,
-            },
-            v6 /*: any*/,
-          ],
-          storageKey: null,
-        },
-      ],
-      type: "Query",
-      abstractKey: null,
-    },
-    kind: "Request",
-    operation: {
-      argumentDefinitions: [],
-      kind: "Operation",
-      name: "pageChargesQuery",
-      selections: [
-        {
-          alias: null,
-          args: v7 /*: any*/,
-          concreteType: "ChargeConnection",
-          kind: "LinkedField",
-          name: "charges",
-          plural: false,
-          selections: [
-            {
-              alias: null,
-              args: null,
-              concreteType: "ChargeEdge",
-              kind: "LinkedField",
-              name: "edges",
-              plural: true,
-              selections: [
-                {
-                  alias: null,
-                  args: null,
-                  concreteType: "Charge",
-                  kind: "LinkedField",
-                  name: "node",
-                  plural: false,
-                  selections: [
-                    v0 /*: any*/,
-                    v1 /*: any*/,
-                    {
-                      alias: null,
-                      args: null,
-                      concreteType: "Product",
-                      kind: "LinkedField",
-                      name: "product",
-                      plural: false,
-                      selections: [v2 /*: any*/, v3 /*: any*/, v8 /*: any*/],
-                      storageKey: null,
-                    },
-                    v8 /*: any*/,
-                    v4 /*: any*/,
-                  ],
-                  storageKey: null,
-                },
-                v5 /*: any*/,
-              ],
-              storageKey: null,
-            },
-            v6 /*: any*/,
-          ],
-          storageKey: "charges(first:100)",
-        },
-        {
-          alias: null,
-          args: v7 /*: any*/,
-          filters: null,
-          handle: "connection",
-          key: "ChargeList_charges",
-          kind: "LinkedHandle",
-          name: "charges",
-        },
-      ],
-    },
-    params: {
-      cacheID: "1820a1031476f1c01011fbf8a35acea2",
-      id: null,
-      metadata: {
-        connection: [
+        "alias": "charges",
+        "args": null,
+        "concreteType": "ChargeConnection",
+        "kind": "LinkedField",
+        "name": "__ChargeList_charges_connection",
+        "plural": false,
+        "selections": [
           {
-            count: null,
-            cursor: null,
-            direction: "forward",
-            path: ["charges"],
+            "alias": null,
+            "args": null,
+            "concreteType": "ChargeEdge",
+            "kind": "LinkedField",
+            "name": "edges",
+            "plural": true,
+            "selections": [
+              {
+                "alias": null,
+                "args": null,
+                "concreteType": "Charge",
+                "kind": "LinkedField",
+                "name": "node",
+                "plural": false,
+                "selections": [
+                  (v0/*: any*/),
+                  (v1/*: any*/),
+                  {
+                    "alias": null,
+                    "args": null,
+                    "concreteType": "Product",
+                    "kind": "LinkedField",
+                    "name": "product",
+                    "plural": false,
+                    "selections": [
+                      (v2/*: any*/),
+                      (v3/*: any*/)
+                    ],
+                    "storageKey": null
+                  },
+                  (v4/*: any*/)
+                ],
+                "storageKey": null
+              },
+              (v5/*: any*/)
+            ],
+            "storageKey": null
           },
+          (v6/*: any*/)
         ],
+        "storageKey": null
+      }
+    ],
+    "type": "Query",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": [],
+    "kind": "Operation",
+    "name": "pageChargesQuery",
+    "selections": [
+      {
+        "alias": null,
+        "args": (v7/*: any*/),
+        "concreteType": "ChargeConnection",
+        "kind": "LinkedField",
+        "name": "charges",
+        "plural": false,
+        "selections": [
+          {
+            "alias": null,
+            "args": null,
+            "concreteType": "ChargeEdge",
+            "kind": "LinkedField",
+            "name": "edges",
+            "plural": true,
+            "selections": [
+              {
+                "alias": null,
+                "args": null,
+                "concreteType": "Charge",
+                "kind": "LinkedField",
+                "name": "node",
+                "plural": false,
+                "selections": [
+                  (v0/*: any*/),
+                  (v1/*: any*/),
+                  {
+                    "alias": null,
+                    "args": null,
+                    "concreteType": "Product",
+                    "kind": "LinkedField",
+                    "name": "product",
+                    "plural": false,
+                    "selections": [
+                      (v2/*: any*/),
+                      (v3/*: any*/),
+                      (v8/*: any*/)
+                    ],
+                    "storageKey": null
+                  },
+                  (v8/*: any*/),
+                  (v4/*: any*/)
+                ],
+                "storageKey": null
+              },
+              (v5/*: any*/)
+            ],
+            "storageKey": null
+          },
+          (v6/*: any*/)
+        ],
+        "storageKey": "charges(first:100)"
       },
-      name: "pageChargesQuery",
-      operationKind: "query",
-      text: "query pageChargesQuery {\n  charges(first: 100) {\n    edges {\n      node {\n        brCode\n        customerEmail\n        product {\n          name\n          price\n          id\n        }\n        id\n        __typename\n      }\n      cursor\n    }\n    pageInfo {\n      endCursor\n      hasNextPage\n    }\n  }\n}\n",
+      {
+        "alias": null,
+        "args": (v7/*: any*/),
+        "filters": null,
+        "handle": "connection",
+        "key": "ChargeList_charges",
+        "kind": "LinkedHandle",
+        "name": "charges"
+      }
+    ]
+  },
+  "params": {
+    "cacheID": "1820a1031476f1c01011fbf8a35acea2",
+    "id": null,
+    "metadata": {
+      "connection": [
+        {
+          "count": null,
+          "cursor": null,
+          "direction": "forward",
+          "path": [
+            "charges"
+          ]
+        }
+      ]
     },
-  };
+    "name": "pageChargesQuery",
+    "operationKind": "query",
+    "text": "query pageChargesQuery {\n  charges(first: 100) {\n    edges {\n      node {\n        brCode\n        customerEmail\n        product {\n          name\n          price\n          id\n        }\n        id\n        __typename\n      }\n      cursor\n    }\n    pageInfo {\n      endCursor\n      hasNextPage\n    }\n  }\n}\n"
+  }
+};
 })();
 
 (node as any).hash = "7d7aac60bdd9bc951889f092e0822d57";
