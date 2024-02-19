da0c5afc3c08870cdd6990c4975998ff66007d67
Leandro Alencar <zzleandrobritozz@gmail.com>
2024-02-19 09:34:13 -0300

    feat(dashboard): add skeleton for dashboard query

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,7 +1,9 @@
 import Link from "next/link"
 import { Home, Package2Icon, PackageSearch, Settings, ShoppingCart, UsersRound } from "lucide-react"
+import { Suspense } from "react"
 import ProductsDashboard from "@/components/ProductsDashboard"
 import { MenuDrawer } from "@/components/MenuDrawer"
+import DashboardSkeleton from "@/components/DashboardSkeleton"
 
 export default function Component() {
   return (
@@ -78,7 +80,9 @@ export default function Component() {
         </header>
         <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6">
           <div className="p-2">
-            <ProductsDashboard />
+            <Suspense fallback={<DashboardSkeleton />}>
+              <ProductsDashboard />
+            </Suspense>
           </div>
         </main>
       </div>
